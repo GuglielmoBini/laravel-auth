2f4f3a51ac54ce54502b64e39810a5b81747e82e
GuglielmoBini <guglielmobini@gmail.com>
2023-03-07 18:15:02 +0100

    migrated and seeded

--- database/migrations/2023_03_07_153420_create_projects_table.php
+++ database/migrations/2023_03_07_153420_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50)->unique();
+            $table->text('project_url');
+            $table->text('image_url')->nullable();
+            $table->text('description');
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 5; $i++) {
+            $project = new Project();
+            $project->name = $faker->words(3, true);
+            $project->project_url = $faker->url();
+            $project->image_url = $faker->imageUrl(250, 250);
+            $project->description = $faker->paragraphs(10, true);
+            $project->save();
+        }
     }
 }
