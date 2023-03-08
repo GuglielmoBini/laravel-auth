446512381765559c077e952f6b13cc6f0f69de4d
GuglielmoBini <guglielmobini@gmail.com>
2023-03-08 15:48:41 +0100

    correction to home and profile pages, changed card layout in show

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.app')
-
+@section('title', 'Home')
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
+    <h2 class="fs-1 text-secondary my-4">
         {{ __('Dashboard') }}
     </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <div class="card mt-4 w-75 text-center">
+                <div class="card-header fs-3">{{ __('User Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body fs-5 text-success">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
@@ -22,5 +21,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,23 +3,28 @@
 @section('content')
 <section id="detail">
     <header>
-        <h1 class="my-3">{{ $project->name }}</h1>
+        <h1 class="my-4">{{ $project->name }}</h1>
     </header>
     <div class="d-flex flex-column align-items-center">
-        <div class="card mt-2" style="max-width: 450px;">
-            <img src="{{ $project->image_url }}" class="card-img-top img-fluid" alt="{{ $project->name }}">
-            <div>
-                <div class="card-body">
-                  <p class="card-text">{{ $project->description }}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item"><strong>Created: </strong>{{ $project->created_at }}</li>
-                </ul>
-                <div class="card-body">
-                  <a href="{{ $project->project_url }}" class="card-link">Project link</a>
-                  <a href="{{ $project->image_url }}" class="card-link">Image URL</a>
-                </div>
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-4">
+              <img src="{{ $project->image_url }}" class="img-fluid w-100 h-100 rounded-start" alt="{{ $project->name }}">
             </div>
+            <div class="col-8">
+              <div class="row g-0 h-100 flex-column">
+                <div class="card-body d-flex flex-column">
+                  <h5 class="card-title">Descrizione</h5>
+                  <p class="card-text flex-grow-1">{{ $project->description }}</p>
+                  <p class="card-text"><small class="text-muted"><strong>Created: </strong>{{ $project->created_at }}</small></p>
+                </div>
+                <div class="card-body border-top d-flex align-items-center justify-content-around">
+                  <a href="{{ $project->project_url }}">Project link</a>
+                  <a href="{{ $project->image_url }}">Image URL</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <a href="{{ route('admin.projects.index') }}" class="btn btn-danger w-25 my-4">Torna Indietro</a>
     </div>
